d519d54093ad9b0721bc53bc14e5ef08ff5519bc
Maso79 <fumagalli.tommaso1@gmail.com>
2022-06-17 16:49:05 +0200

    link

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,19 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //indirizzo in locale del frontend
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); 
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/routest', (req, res) => {
+  res.send("ok")
+})
+
 app.listen(PORT, () => {
-  console.log("OK");
+  console.log(`Listening on port ${PORT}`);
 });
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+
 const Home: React.FC = () => {
-  return <h1>test</h1>;
+  
+  useEffect(() => {
+    test()  
+  }, [])
+
+  async function test() {
+    fetch('/routest')
+      .then(response => response.json())
+      .then(res => console.log(res))
+  }
+
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
 };
 
 export default Home;
