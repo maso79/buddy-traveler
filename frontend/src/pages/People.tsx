5c42a6674c3c23adb4d9614de06c62451c77572a
mb-code-g <mb.code.profile@gmail.com>
2022-09-16 11:49:13 +0200

    Ricerca username ok

--- frontend/src/pages/People.tsx
+++ frontend/src/pages/People.tsx
@@ -7,26 +7,35 @@ import PeopleSuggestions from '../components/PeopleSuggestions';
 const People: React.FC=()=>{
     const [suggestions,setSuggestions]=React.useState([{profilePicture: "", username: ""}])
     const [query,setQuery]=React.useState("")
+    const [httpRequestCalled,setHttpRequestCalled]=React.useState(false)
+    var searchTimeout=setTimeout(()=>{},100)
 
     React.useEffect(()=>{
         setSuggestions([])
-    },[])
+        clearTimeout(searchTimeout)
+        searchTimeout=setTimeout(async ()=>{
+            if (query.length>=3){
+                const result=await fetch("/people/find",{
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "Application/JSON"
+                    },
+                    body: JSON.stringify({letters: query})
+                })
+                .then(result=>result.json())
+                .then(result=>{
+                    setSuggestions(result.stato)
+                    setHttpRequestCalled(true)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+            }
+        },500)
+    },[query])
 
     const lookForPeople=async ()=>{
-        const result=await fetch("/people/find",{
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/JSON"
-            },
-            body: JSON.stringify({letters: query})
-        })
-        .then(result=>result.json())
-        .then(result=>{
-            setSuggestions(result.stato)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+
     }
 
     return(
@@ -37,8 +46,6 @@ const People: React.FC=()=>{
                     <IonRow>
                         <IonCol size="12">
                             <IonSearchbar placeholder="Search for people" value={query} onIonChange={e=>{
-                                setSuggestions([])
-                                lookForPeople()
                                 setQuery(e.detail.value!)
                             }} />
                             {
